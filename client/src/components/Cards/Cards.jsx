e895a29114445800268b86e8113784641f01689a
NoistNT <arielgnr23@gmail.com>
2023-07-24 19:09:14 -0300

    update lazy load in cards component

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,36 +1,37 @@
 /* eslint-disable react/prop-types */
 import { CardsContainer } from '../StyledComponents/StyledCards'
 import Card from '../Card/Card'
+import { useCallback, useEffect } from 'react'
 
 export default function Cards({ pokemons }) {
-  const lazyImages = document.querySelectorAll('.lazy')
-
-  const options = {
-    root: null, // Use the viewport as the root
-    rootMargin: '0px',
-    threshold: 0.1 // Specify the threshold for intersection
-  }
-
   const handleIntersection = (entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         const img = entry.target
-        const src = img.getAttribute('data-src')
-
         // Replace the placeholder with the actual image source
-        img.src = src
+        img.src = img.dataset.src
 
-        // Stop observing the image
         observer.unobserve(img)
       }
     })
   }
 
-  const observer = new IntersectionObserver(handleIntersection, options)
+  const observeImages = useCallback(() => {
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    })
 
-  lazyImages.forEach((image) => {
-    observer.observe(image)
-  })
+    const lazyImages = document.querySelectorAll('.lazy')
+    lazyImages.forEach((image) => {
+      observer.observe(image)
+    })
+  }, [])
+
+  useEffect(() => {
+    observeImages()
+  }, [observeImages])
 
   return (
     <CardsContainer>
