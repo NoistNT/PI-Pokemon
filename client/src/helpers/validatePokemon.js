16a5023122f6a5dcddfae74d813c6cc3e4a07a80
NoistNT <arielgnr23@gmail.com>
2023-07-25 12:44:55 -0300

    Fix error message in form. It validates only one input at a time

--- client/src/helpers/validatePokemon.js
+++ client/src/helpers/validatePokemon.js
@@ -1,74 +1,66 @@
-export const validatePokemon = (pokemon) => {
+import { capitalize } from './helpers'
+
+const HP = 'hp'
+const NAME = 'name'
+const ATTACK = 'attack'
+const DEFENSE = 'defense'
+const SPEED = 'speed'
+const HEIGHT = 'height'
+const WEIGHT = 'weight'
+const IMAGE = 'image'
+const TYPE = 'type'
+
+export const validatePokemon = (field, value) => {
   const errors = {}
 
-  if (!pokemon.name) {
-    errors.name = 'Name is required'
-  } else if (!/^[a-zA-Z]+(?: [a-zA-Z]+)?$/.test(pokemon.name)) {
-    errors.name = 'Name can only contain letters and spaces'
-  } else if (pokemon.name.length > 20) {
-    errors.name = 'Name must be less than 20 characters'
-  }
+  switch (field) {
+    case NAME:
+      if (!value) {
+        errors.name = 'Name is required'
+      } else if (!/^[a-zA-Z]+(?: [a-zA-Z]+)?$/.test(value)) {
+        errors.name = 'Name can only contain letters'
+      } else if (value.length > 20) {
+        errors.name = 'Name must be less than 20 characters'
+      }
+      break
 
-  if (!pokemon.hp) {
-    errors.hp = 'HP is required'
-  } else if (isNaN(pokemon.hp)) {
-    errors.hp = 'HP must be a number'
-  } else if (pokemon.hp < 0) {
-    errors.hp = 'HP must be a positive number'
-  }
+    case HP:
+    case ATTACK:
+    case DEFENSE:
+    case SPEED:
+    case HEIGHT:
+    case WEIGHT:
+      if (!value) {
+        errors[field.toLowerCase()] = `${capitalize(field)} is required`
+      } else if (isNaN(value)) {
+        errors[field.toLowerCase()] = `${capitalize(field)} must be a number`
+      } else if (value < 0) {
+        errors[field.toLowerCase()] = `${capitalize(
+          field
+        )} must be a positive number`
+      }
+      break
 
-  if (!pokemon.attack) {
-    errors.attack = 'Attack is required'
-  } else if (isNaN(pokemon.attack)) {
-    errors.attack = 'Attack must be a number'
-  } else if (pokemon.attack < 0) {
-    errors.attack = 'Attack must be a positive number'
-  }
+    case IMAGE:
+      if (!value) {
+        errors.image = 'Image URL is required'
+      } else if (
+        !/^https?:\/\/[\w\\-]+(\.[\w\\-]+)+(?:[/#?][\w\-./?=&%]*)?(?:\.(?:jpg|jpeg|png|gif|bmp|svg))$/.test(
+          value
+        )
+      ) {
+        errors.image = 'Invalid image URL'
+      }
+      break
 
-  if (!pokemon.defense) {
-    errors.defense = 'Defense is required'
-  } else if (isNaN(pokemon.defense)) {
-    errors.defense = 'Defense must be a number'
-  } else if (pokemon.defense < 0) {
-    errors.defense = 'Defense must be a positive number'
-  }
+    case TYPE:
+      if (value.length === 0) {
+        errors.type = 'Type is required'
+      }
+      break
 
-  if (!pokemon.speed) {
-    errors.speed = 'Speed is required'
-  } else if (isNaN(pokemon.speed)) {
-    errors.speed = 'Speed must be a number'
-  } else if (pokemon.speed < 0) {
-    errors.speed = 'Speed must be a positive number'
-  }
-
-  if (!pokemon.height) {
-    errors.height = 'Height is required'
-  } else if (isNaN(pokemon.height)) {
-    errors.height = 'Height must be a number'
-  } else if (pokemon.height < 0) {
-    errors.height = 'Height must be a positive number'
-  }
-
-  if (!pokemon.weight) {
-    errors.weight = 'Weight is required'
-  } else if (isNaN(pokemon.weight)) {
-    errors.weight = 'Weight must be a number'
-  } else if (pokemon.weight < 0) {
-    errors.weight = 'Weight must be a positive number'
-  }
-
-  if (!pokemon.image) {
-    errors.image = 'Image URL is required'
-  } else if (
-    !/^https?:\/\/[\w\\-]+(\.[\w\\-]+)+(?:[/#?][\w\-./?=&%]*)?(?:\.(?:jpg|jpeg|png|gif|bmp|svg))$/.test(
-      pokemon.image
-    )
-  ) {
-    errors.image = 'Invalid image URL'
-  }
-
-  if (!pokemon.type || pokemon.type.length === 0) {
-    errors.type = 'Type is required'
+    default:
+      break
   }
 
   return errors
