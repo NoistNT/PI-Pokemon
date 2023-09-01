71882056b2cd6363276ca8c8700b7c1486e73d89
NoistNT <arielgnr23@gmail.com>
2023-09-01 09:57:15 -0300

    fix bug in filters

--- client/src/redux/reducers/pokemonReducer.js
+++ client/src/redux/reducers/pokemonReducer.js
@@ -10,15 +10,13 @@ import {
   GET_POKEMON_BY_NAME_PENDING,
   GET_POKEMON_BY_NAME_FULFILLED,
   GET_POKEMON_BY_NAME_REJECTED,
-  GET_POKEMONS_BY_TYPE_PENDING,
-  GET_POKEMONS_BY_TYPE_FULFILLED,
-  GET_POKEMONS_BY_TYPE_REJECTED,
   GET_POKEMONS_BY_TYPE,
   GET_POKEMONS_BY_SOURCE,
   POST_POKEMON_PENDING,
   POST_POKEMON_FULFILLED,
   POST_POKEMON_REJECTED,
   GET_TYPES,
+  SET_CURRENT_PAGE,
   SORT_POKEMONS,
   CLEAN_DETAIL,
   RESET_FILTERS
@@ -28,42 +26,46 @@ const initialState = {
   allPokemons: [],
   pokemons: [],
   pokemon: {},
-  unsortedPokemons: [],
   types: [],
   isLoading: true,
   error: null,
-  success: false
+  sort: 'asc',
+  filter: '',
+  source: '',
+  currentPage: 1
 }
 
 export const pokemonReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case GET_POKEMONS_PENDING:
+    case GET_POKEMON_BY_ID_PENDING:
+    case GET_POKEMON_BY_NAME_PENDING:
+    case POST_POKEMON_PENDING:
       return {
         ...state,
         isLoading: true,
         error: null
       }
-    case GET_POKEMONS_FULFILLED:
-      return {
-        ...state,
-        allPokemons: payload,
-        pokemons: payload,
-        unsortedPokemons: payload,
-        isLoading: false,
-        error: null
-      }
+
     case GET_POKEMONS_REJECTED:
+    case GET_POKEMON_BY_ID_REJECTED:
+    case GET_POKEMON_BY_NAME_REJECTED:
+    case POST_POKEMON_REJECTED:
       return {
         ...state,
         isLoading: false,
         error: payload
       }
-    case GET_POKEMON_BY_ID_PENDING:
+
+    case GET_POKEMONS_FULFILLED:
       return {
         ...state,
-        isLoading: true,
+        allPokemons: payload,
+        pokemons: payload,
+        isLoading: false,
         error: null
       }
+
     case GET_POKEMON_BY_ID_FULFILLED:
       return {
         ...state,
@@ -71,18 +73,7 @@ export const pokemonReducer = (state = initialState, { type, payload }) => {
         isLoading: false,
         error: null
       }
-    case GET_POKEMON_BY_ID_REJECTED:
-      return {
-        ...state,
-        isLoading: false,
-        error: payload
-      }
-    case GET_POKEMON_BY_NAME_PENDING:
-      return {
-        ...state,
-        isLoading: true,
-        error: null
-      }
+
     case GET_POKEMON_BY_NAME_FULFILLED:
       return {
         ...state,
@@ -90,110 +81,95 @@ export const pokemonReducer = (state = initialState, { type, payload }) => {
         isLoading: false,
         error: null
       }
-    case GET_POKEMON_BY_NAME_REJECTED:
-      return {
-        ...state,
-        isLoading: false,
-        error: payload
-      }
-    // Filter pokemons where the given type by the payload is included
-    // Use this to filter from the state
+
     case GET_POKEMONS_BY_TYPE:
-      const pokemonsByType = [...state.allPokemons].filter((pokemon) =>
+      const pokemonsFoundByType = [...state.allPokemons].filter((pokemon) =>
         pokemon.type.includes(payload)
       )
 
-      if (!pokemonsByType.length) {
+      if (!pokemonsFoundByType.length) {
         return {
           ...state,
-          pokemons: [],
           isLoading: false,
-          error: `${capitalize(payload)} pokémons not found`
+          error: `${capitalize(payload)} pokémons not found`,
+          filter: payload
         }
       }
 
       return {
         ...state,
-        pokemons: pokemonsByType,
+        pokemons: pokemonsFoundByType,
         isLoading: false,
-        error: null
+        error: null,
+        filter: payload
       }
-    case GET_POKEMONS_BY_TYPE_PENDING:
-      return {
-        ...state,
-        isLoading: true,
-        error: null
-      }
-    // Filter pokemons from server side
-    case GET_POKEMONS_BY_TYPE_FULFILLED:
-      return {
-        ...state,
-        pokemons: payload,
-        isLoading: false,
-        error: null
-      }
-    case GET_POKEMONS_BY_TYPE_REJECTED:
-      return {
-        ...state,
-        isLoading: false,
-        error: payload
-      }
-    case GET_POKEMONS_BY_SOURCE:
-      const isSourceFromDb = isNaN(payload)
-      const filteredPokemons = state.allPokemons.filter((pokemon) =>
-        isSourceFromDb ? isNaN(pokemon.id) : !isNaN(pokemon.id)
-      )
-      return {
-        ...state,
-        pokemons: filteredPokemons,
-        isLoading: false,
-        error: null
-      }
-    case POST_POKEMON_PENDING:
-      return {
-        ...state,
-        isLoading: true,
-        error: null
-      }
+
     case POST_POKEMON_FULFILLED:
       return {
         ...state,
         isLoading: false,
-        error: null,
-        success: true
+        error: null
       }
-    case POST_POKEMON_REJECTED:
-      return {
-        ...state,
-        isLoading: false,
-        error: payload
-      }
+
     case GET_TYPES:
       return {
         ...state,
         types: payload,
         isLoading: false
       }
+
     case SORT_POKEMONS:
-      const allPokemonsSorted = sortPokemons(state.allPokemons, payload)
-      const pokemonsSorted = sortPokemons(state.pokemons, payload)
+      const sortedAllPokemons = sortPokemons(state.allPokemons, payload)
+      const sortedPokemons = state.pokemons.length
+        ? sortPokemons(state.pokemons, payload)
+        : sortedAllPokemons
 
       return {
         ...state,
-        allPokemons: allPokemonsSorted,
-        pokemons: pokemonsSorted,
+        allPokemons: sortedAllPokemons,
+        pokemons: sortedPokemons,
+        error: null,
+        sort: payload
+      }
+
+    case GET_POKEMONS_BY_SOURCE:
+      let filterCondition
+      if (payload === 'database') {
+        filterCondition = (pokemon) => isNaN(pokemon.id)
+      } else if (payload === 'api') {
+        filterCondition = (pokemon) => !isNaN(pokemon.id)
+      } else {
+        filterCondition = () => true
+      }
+
+      const filteredPokemons = state.allPokemons.filter(filterCondition)
+
+      return {
+        ...state,
+        pokemons: filteredPokemons,
+        isLoading: false,
+        error: null,
+        source: payload
+      }
+
+    case SET_CURRENT_PAGE:
+      return {
+        ...state,
+        currentPage: payload,
         error: null
       }
+
     case CLEAN_DETAIL:
       return {
         ...state,
         pokemon: {},
         error: null
       }
+
     case RESET_FILTERS:
       return {
         ...state,
-        pokemons: [...state.unsortedPokemons],
+        pokemons: sortPokemons(state.allPokemons, 'asc'),
         error: null
       }
 
