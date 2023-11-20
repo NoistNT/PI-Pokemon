4caf6669b91cf94014f3ed0930b773c9a7a71ae6
NoistNT <arielgnr23@gmail.com>
2023-11-19 23:14:44 -0300

    add styles to footer

---
+++ src/components/StyledComponents/StyledFooter.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const StyledFooter = styled.footer`
+  text-align: center;
+  line-height: 0.75rem;
+  color: var(--text-color);
+  padding: 1rem 0;
+  opacity: 70%;
+  font-family: 'Open Sans', Inter, system-ui, Avenir, Helvetica, Arial,
+    sans-serif;
+`
