1a4085ade63e0dba04b8dd21e4453662b03a9138
NoistNT <arielgnr23@gmail.com>
2024-02-28 14:48:48 -0300

    Remove unused component

--- src/components/Card/remove-button.jsx
+++
@@ -1,9 +0,0 @@
-import { RemoveButton, RemoveButtonImg } from '../StyledComponents/StyledCard'
-
-export default function Remove({ alt, src, handleRemove }) {
-  return (
-    <RemoveButton type="button" onClick={handleRemove}>
-      <RemoveButtonImg alt={alt} src={src} />
-    </RemoveButton>
-  )
-}
