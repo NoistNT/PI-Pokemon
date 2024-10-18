ba267e2c47826f3535a58d1ed862c4e857582284
NoistNT <arielgnr23@gmail.com>
2024-10-18 13:53:48 -0300

    Rename index in views

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,6 +1,5 @@
 import CreatePokemon from './CreatePokemon/CreatePokemon'
-import Detail from './Detail/Detail'
 import Home from './Home/Home'
 import Landing from './Landing/Landing'
 
-export { CreatePokemon, Detail, Home, Landing }
+export { CreatePokemon, Home, Landing }
