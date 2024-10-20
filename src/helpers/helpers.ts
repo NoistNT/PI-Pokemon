6d8a92022c6c7023e688dad64fb42ea939d0e8f8
NoistNT <arielgnr23@gmail.com>
2024-10-20 15:39:28 -0300

    Add type to helper functions

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,4 +1,4 @@
-import type { Pokemon, Types } from '@/types/types'
+import type { Pokemon, SortOptions, Types } from '@/types/types'
 
 export const capitalize = (name: string) => {
   return name ? name.charAt(0).toUpperCase() + name.slice(1) : ''
@@ -8,21 +8,18 @@ export const types = (types: Types[]) => {
   return types.map(({ name }) => `${capitalize(name)} `)
 }
 
-export const sortPokemons = <T extends keyof typeof sortFuncMap>(
-  pokemons: Pokemon[],
-  sortOption: T
-) => {
-  const sortFuncMap = {
-    asc: (a: Pokemon, b: Pokemon) => a.name.localeCompare(b.name),
-    desc: (a: Pokemon, b: Pokemon) => b.name.localeCompare(a.name),
-    higherAtk: (a: Pokemon, b: Pokemon) => b.attack - a.attack,
-    lowerAtk: (a: Pokemon, b: Pokemon) => a.attack - b.attack,
-    higherDef: (a: Pokemon, b: Pokemon) => b.defense - a.defense,
-    lowerDef: (a: Pokemon, b: Pokemon) => a.defense - b.defense,
-    higherHp: (a: Pokemon, b: Pokemon) => b.hp - a.hp,
-    lowerHp: (a: Pokemon, b: Pokemon) => a.hp - b.hp,
-    higherSpd: (a: Pokemon, b: Pokemon) => b.speed - a.speed,
-    lowerSpd: (a: Pokemon, b: Pokemon) => a.speed - b.speed
+export const sortPokemons = (pokemons: Pokemon[], sortOption: SortOptions) => {
+  const sortFuncMap: Record<SortOptions, (a: Pokemon, b: Pokemon) => number> = {
+    asc: (a, b) => a.name.localeCompare(b.name),
+    desc: (a, b) => b.name.localeCompare(a.name),
+    higherAtk: (a, b) => b.attack - a.attack,
+    lowerAtk: (a, b) => a.attack - b.attack,
+    higherDef: (a, b) => b.defense - a.defense,
+    lowerDef: (a, b) => a.defense - b.defense,
+    higherHp: (a, b) => b.hp - a.hp,
+    lowerHp: (a, b) => a.hp - b.hp,
+    higherSpd: (a, b) => b.speed - a.speed,
+    lowerSpd: (a, b) => a.speed - b.speed
   }
 
   const sortFunc = sortFuncMap[sortOption] || (() => 0)
