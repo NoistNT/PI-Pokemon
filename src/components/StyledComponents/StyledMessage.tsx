6a70a276e154c326368dc8284c558c66d7529804
NoistNT <arielgnr23@gmail.com>
2024-10-20 15:38:09 -0300

    Update styles in message component

--- src/components/StyledComponents/StyledMessage.tsx
+++ src/components/StyledComponents/StyledMessage.tsx
@@ -3,8 +3,9 @@ import styled from 'styled-components'
 export const Message = styled.p`
   display: flex;
   justify-content: center;
+  align-items: center;
   text-align: center;
-  padding-top: 8.5rem;
+  padding: 8.5rem 0;
   font-size: 1.1rem;
   color: var(--text-color);
   font-family: 'Open Sans', Inter, system-ui, Avenir, Helvetica, Arial,
