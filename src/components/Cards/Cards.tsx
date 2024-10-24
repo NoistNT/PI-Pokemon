2686eaa11492c894993d46847d3eff317c1f7dd0
NoistNT <arielgnr23@gmail.com>
2024-10-24 11:10:46 -0300

    Add skeleton card

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,22 +1,29 @@
 import type { Pokemon } from '@/types/types'
 
 import Card from '@/components/Card/Card'
+import { SkeletonCard } from '@/components/Card/SkeletonCard'
 import {
   CardsContainer,
   Container
 } from '@/components/StyledComponents/StyledCards'
+import { POKEMONS_PER_PAGE } from '@/utils/constants'
 
 interface Props {
   pokemons: Pokemon[]
 }
 
 export default function Cards({ pokemons }: Props) {
+  const arr = Array.from({ length: POKEMONS_PER_PAGE }, (_, i) => i)
+  const skeletons = arr.map((i) => <SkeletonCard key={i} />)
+
   return (
     <Container>
       <CardsContainer>
-        {pokemons.map((pokemon) => (
-          <Card key={pokemon.id} pokemon={pokemon} />
-        ))}
+        {pokemons.length
+          ? pokemons.map((pokemon) => (
+              <Card key={pokemon.id} pokemon={pokemon} />
+            ))
+          : skeletons}
       </CardsContainer>
     </Container>
   )
---
+++ src/components/StyledComponents/StyledSkeletonCard.tsx
@@ -0,0 +1,75 @@
+import styled, { keyframes } from 'styled-components'
+
+const shimmer = keyframes`
+  0% {
+    background-position: -400px 0;
+  }
+  100% {
+    background-position: 400px 0;
+  }
+`
+
+const SkeletonShimmer = styled.div`
+  background-color: #44475a;
+  background-image: linear-gradient(
+    to right,
+    #44475a 0%,
+    #555861 20%,
+    #44475a 40%,
+    #44475a 100%
+  );
+  background-size: 800px 100%;
+  animation: ${shimmer} 1.6s infinite linear;
+`
+
+export const SkeletonCardContainer = styled.div`
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1.5rem;
+  height: 27.95rem;
+  width: 17.8rem;
+  border-style: solid;
+  border-color: #ffffff36;
+  border-radius: 0.85rem;
+  background: linear-gradient(
+    82.3deg,
+    rgb(50, 39, 66) 0%,
+    rgb(76, 71, 148) 100%
+  );
+  box-shadow: 0 2px 4px var(--card-shadow);
+  transition: all 0.3s ease;
+`
+
+export const SkeletonImage = styled(SkeletonShimmer)`
+  width: 17.8rem;
+  height: 17.8rem;
+  background-color: #44475a;
+  border-radius: 0.5rem;
+`
+
+export const SkeletonName = styled(SkeletonShimmer)`
+  width: 50%;
+  background-color: #44475a;
+  margin: 0 auto;
+  margin-top: auto;
+  padding: 0.87rem 0.5rem;
+  border-radius: 0.4rem;
+`
+
+export const SkeletonTypesContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1.4rem;
+  gap: 0 1rem;
+  padding-bottom: 0.8rem;
+`
+
+export const SkeletonType = styled(SkeletonShimmer)`
+  padding: 1rem 2.8rem;
+  background-color: #44475a;
+  border-radius: 0.3rem;
+`
