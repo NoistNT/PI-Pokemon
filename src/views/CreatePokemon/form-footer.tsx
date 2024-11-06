8cbdf53c7d7b11fcd689ace587e575ecd985c705
NoistNT <arielgnr23@gmail.com>
2024-11-06 19:59:25 -0300

    Add form-footer component

---
+++ src/views/CreatePokemon/form-footer.tsx
@@ -0,0 +1,34 @@
+import type { Pokemon } from '@/types/types'
+
+import {
+  ButtonsContainer,
+  ButtonText,
+  Spinner,
+  SubmitButton
+} from '@/components/StyledComponents/StyledForm'
+import { emptyErrors, resetErrors, resetPokemonForm } from '@/helpers/helpers'
+
+interface Props {
+  isLoading: boolean
+  setErrors: React.Dispatch<React.SetStateAction<typeof emptyErrors>>
+  setPokemon: React.Dispatch<React.SetStateAction<Pokemon>>
+}
+
+export function FormFooter({ isLoading, setErrors, setPokemon }: Props) {
+  return (
+    <ButtonsContainer>
+      <SubmitButton disabled={isLoading} type="submit">
+        <ButtonText>{isLoading ? <Spinner /> : 'Create Pokémon'}</ButtonText>
+      </SubmitButton>
+      <SubmitButton
+        type="button"
+        onClick={() => {
+          resetErrors(setErrors)
+          resetPokemonForm(setPokemon)
+        }}
+      >
+        <ButtonText>Clear form</ButtonText>
+      </SubmitButton>
+    </ButtonsContainer>
+  )
+}
