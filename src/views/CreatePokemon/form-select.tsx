c5abfd44911feb0fa55987a5d8e97e4658cbbe0f
NoistNT <arielgnr23@gmail.com>
2024-11-06 19:59:10 -0300

    Add form-select component

---
+++ src/views/CreatePokemon/form-select.tsx
@@ -0,0 +1,72 @@
+import type { Pokemon, Types } from '@/types/types'
+
+import {
+  ErrorSpan,
+  FormItem,
+  IconRemoveType,
+  Label,
+  LabelInputContainer,
+  PokemonTypesList,
+  SelectBox,
+  TypeList,
+  TypeListContainer
+} from '@/components/StyledComponents/StyledForm'
+import { capitalize } from '@/helpers/helpers'
+
+import icon_remove from '@/assets/icon_remove.svg'
+
+interface Props {
+  errors: string
+  pokemonType: Types[]
+  setPokemon: React.Dispatch<React.SetStateAction<Pokemon>>
+  types: Types[]
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
+}
+
+export function FormSelect({
+  errors,
+  pokemonType,
+  setPokemon,
+  types,
+  onChange
+}: Props) {
+  const handleRemove = (typeName: string) => {
+    setPokemon((pokemon) => ({
+      ...pokemon,
+      type: pokemon.type.filter(({ name }) => name !== typeName)
+    }))
+  }
+
+  const pokemonTypesList = types.map(({ _id, name }) => (
+    <option key={_id} value={name}>
+      {capitalize(name)}
+    </option>
+  ))
+
+  const pokemonTypesElement = pokemonType.map(({ name }) => {
+    return (
+      <TypeListContainer key={name} onClick={() => handleRemove(name)}>
+        <TypeList key={name}>{capitalize(name)}</TypeList>
+        <IconRemoveType alt="remove" src={icon_remove} />
+      </TypeListContainer>
+    )
+  })
+
+  return (
+    <FormItem>
+      <LabelInputContainer>
+        <Label htmlFor="types">Type: </Label>
+        <SelectBox
+          defaultValue="normal"
+          id="type"
+          name="type"
+          onChange={onChange}
+        >
+          {pokemonTypesList}
+        </SelectBox>
+      </LabelInputContainer>
+      {errors ? <ErrorSpan>{errors}</ErrorSpan> : null}
+      <PokemonTypesList>{pokemonTypesElement}</PokemonTypesList>
+    </FormItem>
+  )
+}
